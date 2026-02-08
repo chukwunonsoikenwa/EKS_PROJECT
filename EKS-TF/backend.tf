d3734c9b38a3d181d719351dadcf86ed39d53fbb
Ikenwa Chukwunonso <nonsoikenwa@gmail.com>
2026-02-08 00:04:27 -0700

    new

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,16 +1,17 @@
 terraform {
-  backend "s3" {
-    bucket       = "dev-nonso-tf-bucket"
-    region       = "us-east-1"
-    key          = "EKS-TF/terraform.tfstate"
-    use_lockfile = true
-    encrypt      = true
-  }
-  required_version = ">=1.14.0"
+  required_version = ">= 1.14.0"
+
   required_providers {
     aws = {
       version = ">= 5.49.0"
       source  = "hashicorp/aws"
     }
+  }
+
+  backend "s3" {
+    bucket = "dev-nonso-tf-bucket"
+    key    = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/EKS-TF/terraform.tfstate"
+    region = "us-east-1"
+    encrypt = true
   }
 }
